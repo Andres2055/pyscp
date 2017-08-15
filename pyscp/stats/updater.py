ff3805cf63741a12be706e4235902595bbab2ec3
Andrés <andrecito104@hotmail.com>
2017-08-15 18:58:32 -0400

    Update updater.py

--- pyscp/stats/updater.py
+++ pyscp/stats/updater.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 """
-Stat Updater.
+Actualizador de Estadísticas.
 
-Calculate stats from one wiki and write it into another.
+Calcula las estadísticas de una wiki y las escribe en otra.
 """
 
 ###############################################################################
-# Module Imports
+# Módulos Importados
 ###############################################################################
 
 import logging
@@ -16,7 +16,7 @@ from pyscp import snapshot, wikidot, utils
 from pyscp.stats import scalars, counters, filters
 
 ###############################################################################
-# Global Constants And Variables
+# Constantes Globales Y Variables
 ###############################################################################
 
 log = logging.getLogger(__name__)
@@ -27,11 +27,11 @@ log = logging.getLogger(__name__)
 class Updater:
 
     scalars_author = (
-        ('Pages Created', len),
-        ('Net Rating', scalars.rating),
-        ('Average Rating', scalars.rating_average),
-        ('Wordcount', scalars.wordcount),
-        ('Average Wordcount', scalars.wordcount_average))
+        ('Páginas Creadas', len),
+        ('Puntaje Neto', scalars.rating),
+        ('Puntaje Medio', scalars.rating_average),
+        ('Recuento de Palabras', scalars.wordcount),
+        ('Recuento de Palabras Medio', scalars.wordcount_average))
 
     def __init__(self, source, target):
         self.pages = list(source.list_pages())
@@ -40,9 +40,9 @@ class Updater:
 
     @staticmethod
     def source_counter(counter):
-        """Build wikidot markup source for ranking pages."""
-        source = ['||~ Rank||~ User||~ Score||']
-        # sort by score, then alphabetically by user
+        """Construye un código de marcado wikidot para el ranking de las páginas."""
+        source = ['||~ Posición||~ Usuario||~ Puntaje||']
+        # ordenado por puntaje, después alfabeticamente por usuarios
         items = sorted(counter.items(), key=lambda x: x[0].lower())
         items = sorted(items, key=lambda x: x[1], reverse=True)
         template = '||{}||[[[user:{}]]]||{}||'
@@ -51,22 +51,22 @@ class Updater:
         return '\n'.join(source)
 
     def source_author(self, user):
-        """Build source code for the user's authorship stats."""
+        """Construye un código fuente para las estadísticas de autoridad de los usuarios."""
         pages = filters.user(self.pages, user)
-        source = ['++ Authorship Statistics']
+        source = ['++ Estadísticas de Autoridad']
         if not pages:
-            source.append('This user have not authored any pages.')
+            source.append('Este usuario no posee autoria de ninguna página.')
             return '\n'.join(source)
         for descr, func in self.scalars_author:
-            text = '[[[ranking:{}]]]:@@{}@@**{}**'.format(
+            text = '[[[posición:{}]]]:@@{}@@**{}**'.format(
                 descr, ' ' * (40 - len(descr)), round(func(pages), 2))
             source.append('{{%s}}' % text)
         return '\n'.join(source)
 
     def post(self, name, source):
-        """Update if exists; create if not; retry if failed."""
+        """Actualiza, si existe; crea, si no; reintenta si falla."""
         p = self.target(name)
-        for _ in range(10):  # retry ten times max
+        for _ in range(10):  # reintenta un máximo de diez veces
             if p.url in self.exist:
                 response = p.edit(source)
             else:
@@ -74,28 +74,28 @@ class Updater:
                 response = p.create(source, title)
             if response['status'] == 'ok':
                 return
-        log.error('Failed to post: %s', name)
+        log.error('Falla al postear: %s', name)
 
     def update_users(self):
-        """Update the stats wiki with the author stats."""
+        """Actualiza las estadísticas de la wiki con las estadísticas de los autores."""
         users = {p.author for p in self.pages}
-        for user in utils.pbar(users, 'UPDATING USER STATS'):
-            self.post('user:' + user, self.source_author(user))
+        for user in utils.pbar(users, 'ACTUALIZANDO ESTADISTICAS DE USUARIOS'):
+            self.post('usuario:' + user, self.source_author(user))
 
     def update_rankings(self):
         for descr, func in utils.pbar(
-                self.scalars_author, 'UPDATING RANKINGS'):
+                self.scalars_author, 'ACTUALIZANDO POSICIÓN'):
             value = self.source_counter(counters.author(self.pages, func))
-            self.post('ranking:' + descr, round(value, 2))
+            self.post('posición:' + descr, round(value, 2))
 
 
 ###############################################################################
 
 if __name__ == "__main__":
     source = snapshot.Wiki(
-        'www.scp-wiki.net', '/home/anqxyr/heap/_scp/scp-wiki.2015-06-23.db')
-    target = wikidot.Wiki('scp-stats')
-    target.auth('placeholder', 'placeholder')
+        'www.lafundacionscp.wikidot.com', '/home/anqxyr/heap/_scp/scp-wiki.2015-06-23.db')
+    target = wikidot.Wiki('scp-es-stats')
+    target.auth('andres2055', 'morrocoy')
     up = Updater(source, target)
     up.update_rankings()
     up.update_users()
