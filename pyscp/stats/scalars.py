c45f2adeec90d906a8cbcdf1ea8a396c2f2ca77a
Andrés <andrecito104@hotmail.com>
2017-08-15 18:58:09 -0400

    Update scalars.py

--- pyscp/stats/scalars.py
+++ pyscp/stats/scalars.py
@@ -7,30 +7,30 @@ Take a list of pages and return a single value.
 """
 
 def upvotes(pages):
-    """Upvotes."""
+    """Votos positivos."""
     return sum([v.value for v in p.votes].count(1) for p in pages)
 
 
 def rating(pages):
-    """Net rating."""
+    """Puntaje neto."""
     return sum(p.rating for p in pages)
 
 
 def rating_average(pages):
-    """Average rating."""
+    """Puntaje medio."""
     return rating(pages) / len(pages)
 
 
 def divided(pages):
-    """Controversy score."""
+    """Puntajes controversiales."""
     return sum(len(p.votes) / p.rating for p in pages)
 
 
 def redactions(pages):
-    """Redaction score."""
+    """Redacción de puntajes."""
     return sum(
         p.text.count('█') +
-        20 * sum(map(p.text.count, ('REDACTED', 'EXPUNGED')))
+        20 * sum(map(p.text.count, ('CENSURADO', 'ELIMINADO')))
         for p in pages)
 
 
